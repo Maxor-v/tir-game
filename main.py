8a1c67f6ca7bc923c33b05058a7144e58d6b4144
Maxor-v <maxim.v.orlov@yandex.ru>
2025-04-06 10:42:46 +0300

    исправлены размеры цели

--- main.py
+++ main.py
@@ -2,35 +2,42 @@ import pygame
 import random
 
 pygame.init()
+
+# Параметры экрана
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 pygame.display.set_caption("Игра Тир")
-icon = pygame.image.load("img/icon.jpg")
+icon = pygame.image.load("img/icon.png")
 pygame.display.set_icon(icon)
 
-target_img = pygame.image.load("img/target.png")
-target_width = 80
-target_height = 80
+target_img_original = pygame.image.load("img/target.png")
+target_width = 50
+target_height = 50
+target_img = pygame.transform.scale(target_img_original, (target_width, target_height))
+
+# Начальные координаты цели
 target_x = random.randint(0, SCREEN_WIDTH - target_width)
 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 
+# Цвет фона
 color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
 running = True
 while running:
-        screen.fill(color)
-        for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                        running = False
+    screen.fill(color)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
-                mouse_x, mouse_y = pygame.mouse.get_pos()
-                if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
-                        target_x = random.randint(0, SCREEN_WIDTH - target_width)
-                        target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+            mouse_x, mouse_y = pygame.mouse.get_pos()
 
-screen.blit(target_img, (target_x, target_y))
-pygame.display.update()
+            if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
+                target_x = random.randint(0, SCREEN_WIDTH - target_width)
+                target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+
+    screen.blit(target_img, (target_x, target_y))
+    pygame.display.update()
 
 pygame.quit()
